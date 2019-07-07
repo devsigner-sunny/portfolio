0430743bf563e4920f65d03af39bc88e1ca5253e
devsigner-sunny <devsigner.sunny@gmail.com>
2019-07-07 19:58:06 +1200

    Updates

---
+++ precache-manifest.cc1a7b02bf5c2f183efa77cef78f2933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c2d56a3b9b0c9fd3302ba815dfca05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b304591e43908c40a33d",
+    "url": "/static/css/main.7e2d7b1b.chunk.css"
+  },
+  {
+    "revision": "96e7f727ee653edabf02",
+    "url": "/static/js/2.ae57344b.chunk.js"
+  },
+  {
+    "revision": "b304591e43908c40a33d",
+    "url": "/static/js/main.c4d89114.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
